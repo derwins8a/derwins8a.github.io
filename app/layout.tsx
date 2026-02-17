9ed052cf3d56e184eeb3f82c54bf703b7fb0d92e
derwins8a <111207083+derwins8a@users.noreply.github.com>
2026-02-17 00:45:38 -0400

    Add shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import { getPageMap } from 'nextra/page-map'
 import type { FC, ReactNode } from 'react'
 import { NextraTheme } from './_components/nextra-theme'
 import './globals.css'
+import { Geist } from "next/font/google";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: {
@@ -15,7 +18,7 @@ export const metadata: Metadata = {
 const RootLayout: FC<{ children: ReactNode }> = async ({ children }) => {
   const pageMap = await getPageMap()
   return (
-    <html lang="en" dir="ltr">
+    <html lang="en" dir="ltr" className={geist.variable}>
       <Head faviconGlyph="✦" />
       <body style={{ margin: 0 }}>
         <NextraTheme pageMap={pageMap}>{children}</NextraTheme>
